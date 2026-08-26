9674acae8de7c22727ba1576ed6155d9dbfeaa72
WaveF <wavef@qq.com>
2026-08-27 01:07:45 +0800

    refactor(markdown): remove built-in AI panel

--- apps/markdown/src/renderer/components/Ribbon.tsx
+++ apps/markdown/src/renderer/components/Ribbon.tsx
@@ -5,7 +5,6 @@ import { useEditorState } from '@tiptap/react'
 import { Dropdown, useDismissablePopover } from '@genoffice/ui'
 import { useI18n } from '../i18n/locale'
 import type { StringKey } from '../i18n/locale'
-import { GensparkMark } from '../ai/AiPanel'
 import { liftFromList } from '../editor/slashCommand'
 import {
   IconBullets,
@@ -296,7 +295,7 @@ export function Ribbon({
               onClick={onToggleAi}
             >
               <span className="rb-big-icon">
-                <GensparkMark size={26} />
+                <IconProperties size={26} />
               </span>
               <span>Genspark AI</span>
             </button>
